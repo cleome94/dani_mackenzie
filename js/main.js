58342f6a14415d411f36a61f4618370e1081cbb8
SYKim <cleome94@naver.com>
2022-12-13 18:20:12 +0900

    메인홈 css,js작업

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    $(".gnb_btn").click(function(){
+        $(".gnb_wrap").animate({width:"toggle"}, 200);
+    });
+    $(".gnb_exit").click(function(){
+        $(".gnb_wrap").animate({width:"toggle"}, 200);
+    });
+    $(".search").click(function(){
+        $(".search_box2").slideToggle();
+    });
+    $(".sch_exit").click(function(){
+        $(".search_box2").slideUp();
+    });
+    $('.event ul').bxSlider({
+        auto:true
+    });
+    $(".customer h2").click(function(){
+        $(".customer ul").slideToggle("fast");
+    });
+    $(".cs").click(function(){
+        $(this).toggleClass("on off");
+    });
+});
