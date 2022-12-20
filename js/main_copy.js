3d044711312529655c5a6a394180863dcc53c7cb
SYKim <cleome94@naver.com>
2022-12-20 18:19:43 +0900

    index파일 반응형 모바일로 바꿈

---
+++ js/main_copy.js
@@ -0,0 +1,55 @@
+$(window).resize(function(){
+    if(window.innerWidth > 768){
+        $(".gnb_btn").click(function(){
+            $(".gnb_wrap").animate({width:"toggle"}, 200);
+        });
+        $(".f_m1").click(function(){
+            $(".gnb_wrap").animate({width:"toggle"}, 200);
+        });
+        $(".gnb_exit").click(function(){
+            $(".gnb_wrap").animate({width:"toggle"}, 200);
+        });
+        $(".search").click(function(){
+            $(".search_box2").slideToggle();
+        });
+        $(".sch_exit").click(function(){
+            $(".search_box2").slideUp();
+        });
+        $('.event ul').bxSlider({
+            auto:true
+        });
+    } else{
+        $(".gnb_btn").click(function(){
+            $(".gnb_wrap").animate({width:"toggle"}, 200);
+        });
+        $(".f_m1").click(function(){
+            $(".gnb_wrap").animate({width:"toggle"}, 200);
+        });
+        $(".gnb_exit").click(function(){
+            $(".gnb_wrap").animate({width:"toggle"}, 200);
+        });
+        $(".search").click(function(){
+            $(".search_box2").slideToggle();
+        });
+        $(".sch_exit").click(function(){
+            $(".search_box2").slideUp();
+        });
+        $('.event ul').bxSlider({
+            auto:true
+        });
+        $(".customer h2").click(function(){
+            $(".customer ul").slideToggle("fast");
+        });
+        $(".cs").click(function(){
+            $(this).toggleClass("on off");
+        });
+        $(".cs").trigger("click");
+        $(".f_info h2").click(function(){
+            $(".f_info div").slideToggle("fast");
+        });
+        $(".comp").click(function(){
+            $(this).toggleClass("on off");
+        });
+        $(".comp").trigger("click");
+    }
+}).resize();
